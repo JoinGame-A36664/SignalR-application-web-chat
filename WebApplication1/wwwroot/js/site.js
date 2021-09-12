40122d59d33d7dfde8013173ee0293f2b84a1671
a36664 Nguyễn Văn Lực 😀 <a36664@thanglong.edu.vn>
2021-09-13 00:36:11 +0700

    design ui app chat with signalr

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function (){
+
+    $("#emojibtn").click(function () {
+        $("#emojis-container").toggleClass("d-none");
+    });
+
+})
